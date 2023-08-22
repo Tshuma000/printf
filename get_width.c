b02c4adb88bb4196bc4c6d428ba45e74b4059fc0
Beloved Phakoe <Beloved.phakoe@outlook.com>
2023-08-22 12:07:06 -0700

    error update

--- get_width.c
+++ get_width.c
@@ -4,11 +4,11 @@
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @print_list: list of arguments.
  *
  * Return: width.
  */
-int get_width(const char *format, int *i, va_list list)
+int get_width(const char *format, int *i, va_list print_list)
 {
 	int curr_i;
 	int width = 0;
@@ -23,7 +23,7 @@ int get_width(const char *format, int *i, va_list list)
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			width = va_arg(list, int);
+			width = va_arg(print_list, int);
 			break;
 		}
 		else
